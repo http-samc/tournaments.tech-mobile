0580be9fe76b282b3d8e7627ac432d31abbe1e47
http-samc <67826352+http-samc@users.noreply.github.com>
2021-12-14 19:42:03 -0600

    streamlined colors

--- theme/colors.js
+++ theme/colors.js
@@ -4,11 +4,9 @@ var Colors = {
     secondary: '#0dcaf0',
     textImportant: '#0d6efd',
     background: '#121212',
-    //background: '#2c3440',
     error: '#cf6679',
     onPrimary: '#000000',
     onBackground: '#222',
-    //onBackground: '#212529'
 }
 
 export default Colors = Colors
